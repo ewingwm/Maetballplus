caf18243965a69cc62696bfdbe6cc8c1ed445f2a
ewingwm <65508610+ewingwm@users.noreply.github.com>
2020-09-28 15:25:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   session_start();
   require_once 'header.php';
 
-  echo "<div class='center'>Welcome to Robin's Nest,";
+  echo "<div class='center'>Welcome to TE net,";
 
   if ($loggedin) echo " $user, you are logged in";
   else           echo ' please sign up or log in';
